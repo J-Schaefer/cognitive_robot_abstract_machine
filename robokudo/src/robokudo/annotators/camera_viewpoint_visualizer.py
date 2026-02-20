1d7e0f6fca3f24d4a4c67eadd3867160ac73f295
fmuehlis <fmuehlis@posteo.de>
2026-02-20 17:20:37 +0100

    refactor: improve type hinting and imports in annotators/camera_viewpoint_visualizer.py

--- robokudo/src/robokudo/annotators/camera_viewpoint_visualizer.py
+++ robokudo/src/robokudo/annotators/camera_viewpoint_visualizer.py
@@ -3,19 +3,18 @@
 This module provides an annotator for visualizing camera viewpoints and reference frames
 in 3D space using Open3D.
 """
-import numpy
+
+import numpy as np
 import open3d as o3d
-import py_trees
 
-import robokudo.annotators
-import robokudo.annotators.core
-import robokudo.annotators.outputs
-import robokudo.types.tf
-import robokudo.utils.transform
-from robokudo.cas import CASViews
+from py_trees.common import Status
+from . import core
+from ..types.tf import StampedTransform
+from ..utils.transform import get_transform_matrix_from_q
+from ..cas import CASViews
 
 
-class CameraViewpointVisualizer(robokudo.annotators.core.BaseAnnotator):
+class CameraViewpointVisualizer(core.BaseAnnotator):
     """Annotator for visualizing camera viewpoints and reference frames.
 
     This annotator displays the reference frame set in the viewpoint (e.g., /map frame)
@@ -23,21 +22,17 @@ class CameraViewpointVisualizer(robokudo.annotators.core.BaseAnnotator):
     coordinate frame and the point cloud in the same space.
 
     The annotator will fail if the required viewpoint transform cannot be found in the CAS.
-
-    :ivar name: Name of the annotator instance
-    :type name: str
     """
 
-    def __init__(self, name="CameraViewpointVisualizer"):
+    def __init__(self, name: str = "CameraViewpointVisualizer"):
         """Initialize the camera viewpoint visualizer.
 
         :param name: Name of the annotator instance, defaults to "CameraViewpointVisualizer"
-        :type name: str, optional
         """
         super().__init__(name)
         self.rk_logger.debug("%s.__init__()" % self.__class__.__name__)
 
-    def update(self):
+    def update(self) -> Status:
         """Update the visualization with the current viewpoint and point cloud data.
 
         Creates a visualization containing:
@@ -46,22 +41,22 @@ class CameraViewpointVisualizer(robokudo.annotators.core.BaseAnnotator):
         * The current point cloud data
 
         :return: SUCCESS if visualization was created, FAILURE if viewpoint not found
-        :rtype: py_trees.common.Status
         :raises AssertionError: If viewpoint transform is of a wrong type
         """
         try:
             cam_to_world_transform = self.get_cas().get(CASViews.VIEWPOINT_CAM_TO_WORLD)
         except Exception as err:
             self.rk_logger.warning(f"Couldn't find viewpoint in the CAS: {err}")
-            return py_trees.common.Status.FAILURE
+            return Status.FAILURE
 
-        assert (isinstance(cam_to_world_transform, robokudo.types.tf.StampedTransform))
+        assert isinstance(cam_to_world_transform, StampedTransform)
 
         cloud = self.get_cas().get(CASViews.CLOUD)
         world_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.2)
-        t = robokudo.utils.transform.get_transform_matrix_from_q(cam_to_world_transform.rotation,
-                                                                 cam_to_world_transform.translation)
-        t = numpy.linalg.inv(t)
+        t = get_transform_matrix_from_q(
+            cam_to_world_transform.rotation, cam_to_world_transform.translation
+        )
+        t = np.linalg.inv(t)
         world_frame.transform(t)
 
         geometries = [
@@ -70,4 +65,4 @@ class CameraViewpointVisualizer(robokudo.annotators.core.BaseAnnotator):
         ]
         self.get_annotator_output_struct().set_geometries(geometries)
 
-        return py_trees.common.Status.SUCCESS
+        return Status.SUCCESS
