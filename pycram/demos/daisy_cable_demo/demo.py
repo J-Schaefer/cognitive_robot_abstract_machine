779232e65c66e9769415c9e5e9bfaf21cbaa9f72
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2026-06-07 17:13:08 +0200

    Set up minimal pick and place demo with DAiSy and bowl

--- pycram/demos/daisy_cable_demo/demo.py
+++ pycram/demos/daisy_cable_demo/demo.py
@@ -1,29 +1,35 @@
 import os
 
 from pycram.datastructures.dataclasses import Context
+from pycram.datastructures.grasp import GraspDescription
 from pycram.motion_executor import real_robot, simulated_robot
-from pycram.datastructures.enums import Arms
+from pycram.datastructures.enums import Arms, ApproachDirection, VerticalAlignment
 import rclpy
 
 from pycram.plans.factories import sequential
 from pycram.plans.plan import Plan
+from pycram.robot_plans.actions.core.pick_up import PickUpAction
+from pycram.robot_plans.actions.core.placing import PlaceAction
 from pycram.testing import setup_world
 from pycram.language import SequentialNode
 from pycram.robot_plans.actions.core.robot_body import ParkArmsAction
+from pycram.view_manager import ViewManager
 from semantic_digital_twin.adapters.mesh import STLParser
 from semantic_digital_twin.adapters.ros.visualization.viz_marker import (
     VizMarkerPublisher,
 )
 from semantic_digital_twin.adapters.urdf import URDFParser
+from semantic_digital_twin.robots.abstract_robot import Manipulator
 from semantic_digital_twin.robots.daisy import DAiSy
+from semantic_digital_twin.robots.robot_mixins import SpecifiesLeftRightArm
 from semantic_digital_twin.spatial_types import HomogeneousTransformationMatrix
 from semantic_digital_twin.world_description.connections import FixedConnection
 from test.conftest import world_with_urdf_factory
 
-# %% Environmnent Setup
-environment_path = os.path.join("package://iai_apartment/urdf/apartment.urdf")
-environment_parser = URDFParser.from_file(file_path=environment_path)
-environment_world = environment_parser.parse()
+# %% Environment Setup
+apartment_path = os.path.join("package://iai_apartment/urdf/apartment.urdf")
+apartment_parser = URDFParser.from_file(file_path=apartment_path)
+apartment_world = apartment_parser.parse()
 
 # %% Robot Setup
 daisy = "package://iai_daisy_description/robots/daisy.urdf.xacro"
@@ -39,13 +45,17 @@ bowl = STLParser(
 
 world = daisy_world
 
-world.merge_world(environment_world)
-
 with world.modify_world():
+    world.merge_world_at_pose(
+        apartment_world,
+        HomogeneousTransformationMatrix.from_xyz_rpy(
+            2, -4, 0, 0, 0, 3.14 / 2, reference_frame=world.root
+        ),
+    )
     world.merge_world_at_pose(
         bowl,
         HomogeneousTransformationMatrix.from_xyz_quaternion(
-            2.4, 2.2, 1, reference_frame=world.root
+            -0.4, -0.1, 0.635, reference_frame=world.root
         ),
     )
 
@@ -66,8 +76,27 @@ context = Context.from_world(world)
 
 print(world.root.name)
 
+pick_up_grasp = GraspDescription(
+    approach_direction=ApproachDirection.FRONT,
+    vertical_alignment=VerticalAlignment.TOP,
+    manipulator=context.robot.left_arm.manipulator,
+)
+
 with simulated_robot:
-    plan = sequential([ParkArmsAction(arm=Arms.BOTH)], context)
+    plan = sequential(
+        [
+            ParkArmsAction(arm=Arms.BOTH),
+            PickUpAction(world.get_body_by_name("bowl.stl"), Arms.LEFT, pick_up_grasp),
+            PlaceAction(
+                world.get_body_by_name("bowl.stl"),
+                HomogeneousTransformationMatrix.from_xyz_rpy(
+                    -0.6, -0.1, 0.635, reference_frame=world.root
+                ).to_pose(),
+                Arms.LEFT,
+            ),
+        ],
+        context,
+    )
     # pose_T_object
     # world.transform(pose_T_object, world.root)
     plan.perform()
