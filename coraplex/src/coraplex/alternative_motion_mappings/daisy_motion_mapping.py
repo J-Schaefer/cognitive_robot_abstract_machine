6204d99b346ad4252a554dcfce2f3eeca76e8a61
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2026-06-18 17:25:34 +0200

    Add alternative motion mappings for DAiSy
    Implemented correct message types for the action.

--- coraplex/src/coraplex/alternative_motion_mappings/daisy_motion_mapping.py
+++ coraplex/src/coraplex/alternative_motion_mappings/daisy_motion_mapping.py
@@ -1,13 +1,14 @@
+import logging
+
 from semantic_digital_twin.datastructures.definitions import GripperState
 
-try:
-    from nav2_msgs.action import NavigateToPose
-except ModuleNotFoundError:
-    NavigateToPose = None
+from griplink_interfaces.action import Grip, Release, Flexgrip, Flexrelease
+
 from giskardpy.motion_statechart.ros2_nodes.ros_tasks import (
     NavigateActionServerTask,
     ActionServerTask, WPGGripperActionServerTask,
 )
+
 from semantic_digital_twin.robots.daisy import DAiSy
 from coraplex.datastructures.enums import ExecutionType, Arms
 from coraplex.view_manager import ViewManager
@@ -15,6 +16,7 @@ from coraplex.robot_plans import MoveMotion, MoveToolCenterPointMotion, LookingM
 
 from coraplex.robot_plans.motions.base import AlternativeMotion
 
+logger = logging.getLogger(__name__)
 
 class DAISYGripMotion(MoveGripperMotion, AlternativeMotion[DAiSy]):
     """
@@ -24,6 +26,7 @@ class DAISYGripMotion(MoveGripperMotion, AlternativeMotion[DAiSy]):
     execution_type = ExecutionType.REAL
 
     def perform(self):
+        logger.info(f"Performing action {self.__class__.__name__}")
         return
 
     @property
@@ -32,16 +35,51 @@ class DAISYGripMotion(MoveGripperMotion, AlternativeMotion[DAiSy]):
         if self.gripper == Arms.LEFT:
             if self.motion == GripperState.OPEN:
                 self.action_topic = "/left_gripper/release"
-                self.message_type = NavigateToPose
+                self.message_type = Release
             elif self.motion == GripperState.CLOSE:
                 self.action_topic = "/left_gripper/grip"
+                self.message_type = Grip
             else:
                 raise ValueError(f"Gripper action {self.motion} not supported")
         elif self.gripper == Arms.RIGHT:
             if self.motion == GripperState.OPEN:
                 self.action_topic = "/right_gripper/release"
+                self.message_type = Release
             elif self.motion == GripperState.CLOSE:
                 self.action_topic = "/right_gripper/grip"
+                self.message_type = Grip
+            else:
+                raise ValueError(f"Gripper action {self.motion} not supported")
+        else:
+            raise ValueError(f"Gripper {self.gripper} not supported")
+
+        return WPGGripperActionServerTask(action_topic=self.action_topic, message_type=self.message_type)
+
+class DaisyFlexGripMotion(MoveGripperMotion, AlternativeMotion[DAiSy]):
+    """
+    Use flex grip and release motions for the WPG grippers.
+    """
+
+    def perform(self):
+        return
+
+    def _motion_chart(self) -> ActionServerTask:
+        if self.gripper == Arms.LEFT:
+            if self.motion == GripperState.FLEXCLOSE:
+                self.action_topic = "/left_gripper/flexgrip"
+                self.message_type = Flexgrip
+            elif self.motion == GripperState.FLEXOPEN:
+                self.action_topic = "/left_gripper/flexrelease"
+                self.message_type = Flexrelease
+            else:
+                raise ValueError(f"Gripper action {self.motion} not supported")
+        elif self.gripper == Arms.RIGHT:
+            if self.motion == GripperState.FLEXCLOSE:
+                self.action_topic = "/right_gripper/flexgrip"
+                self.message_type = Flexgrip
+            elif self.motion == GripperState.FLEXOPEN:
+                self.action_topic = "/right_gripper/flexrelease"
+                self.message_type = Flexrelease
             else:
                 raise ValueError(f"Gripper action {self.motion} not supported")
         else:
@@ -49,32 +87,3 @@ class DAISYGripMotion(MoveGripperMotion, AlternativeMotion[DAiSy]):
 
         if self.motion == GripperState.FLEXOPEN or self.motion == GripperState.FLEXCLOSE:
             return WPGGripperActionServerTask(action_topic=self.action_topic, )
-
-    class DaisyFlexGripMotion(MoveGripperMotion, AlternativeMotion[DAiSy]):
-        """
-        Use flex grip and release motions for the WPG grippers.
-        """
-
-        def perform(self):
-            return
-
-        def _motion_chart(self) -> ActionServerTask:
-            if self.gripper == Arms.LEFT:
-                if self.motion == GripperState.FLEXCLOSE:
-                    self.action_topic = "/left_gripper/flexgrip"
-                elif self.motion == GripperState.FLEXOPEN:
-                    self.action_topic = "/left_gripper/flexrelease"
-                else:
-                    raise ValueError(f"Gripper action {self.motion} not supported")
-            elif self.gripper == Arms.RIGHT:
-                if self.motion == GripperState.FLEXCLOSE:
-                    self.action_topic = "/right_gripper/flexgrip"
-                elif self.motion == GripperState.FLEXOPEN:
-                    self.action_topic = "/right_gripper/flexrelease"
-                else:
-                    raise ValueError(f"Gripper action {self.motion} not supported")
-            else:
-                raise ValueError(f"Gripper {self.gripper} not supported")
-
-            if self.motion == GripperState.FLEXOPEN or self.motion == GripperState.FLEXCLOSE:
-                return WPGGripperActionServerTask(action_topic=self.action_topic, )
